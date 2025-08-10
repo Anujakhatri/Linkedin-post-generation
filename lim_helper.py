25766a40346ce71d7bd436237fe248c3d5b46905
Anuja khatri <70836354+Anujakhatri@users.noreply.github.com>
2025-08-10 11:07:47 +0545

    Update lim_helper.py

--- lim_helper.py
+++ lim_helper.py
@@ -4,7 +4,7 @@ from lanchain_groq import ChatGroq
 import os
 load_dotenv()
 
-llm= ChatGroq(groq_api_key= os.getenv("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"), model_name="llama-3.3-70b-versatile")
+llm= ChatGroq(groq_api_key= os.getenv("your_secret_key"), model_name="llama-3.3-70b-versatile")
 if __name__=="__main__":
     llm.invoke("What are the two main ingradients in samosa")
     print(response.content)
