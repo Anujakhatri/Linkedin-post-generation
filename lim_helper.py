c8e367f04861268f037ccd3c0011f5bd7d8bbd1d
Anuja khatri <70836354+Anujakhatri@users.noreply.github.com>
2025-08-10 11:09:37 +0545

    Update lim_helper.py

--- lim_helper.py
+++ lim_helper.py
@@ -6,6 +6,6 @@ load_dotenv()
 
 llm= ChatGroq(groq_api_key= os.getenv("your_secret_key"), model_name="llama-3.3-70b-versatile")
 if __name__=="__main__":
-    llm.invoke("What are the two main ingradients in samosa")
+    llm.invoke("What are the two important method to cook meat")
     print(response.content)
 
