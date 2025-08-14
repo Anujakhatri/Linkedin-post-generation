3e1885ecf09390859ebce075e50a92565525d33c
Anuja khatri <70836354+Anujakhatri@users.noreply.github.com>
2025-08-14 09:42:24 +0545

    Update llm_helper.py

--- llm_helper.py
+++ llm_helper.py
@@ -4,7 +4,7 @@ from langchain_groq import ChatGroq
 import os
 load_dotenv(dotenv_path=".env.save")
 
-llm= ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name ="llama-3.3-70b-versatile")
+llm= ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name ="choose any llm model")
 
 if __name__=="__main__":
     response = llm.invoke("What are the two main ingredients in samosa")
