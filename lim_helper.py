f4f90b844b7d56788435fe06421f54ee1ea5ff9d
Anuja khatri <70836354+Anujakhatri@users.noreply.github.com>
2025-08-14 09:40:29 +0545

    Update lim_helper.py

--- lim_helper.py
+++ lim_helper.py
@@ -4,7 +4,7 @@ from lanchain_groq import ChatGroq
 import os
 load_dotenv()
 
-llm= ChatGroq(groq_api_key= os.getenv("your_secret_key"), model_name="llama-3.3-70b-versatile")
+llm= ChatGroq(groq_api_key= os.getenv("your_secret_key(copy you secret key here)"), model_name="llama-3.3-70b-versatile")
 if __name__=="__main__":
     llm.invoke("What are the two important method to cook meat")
     print(response.content)
