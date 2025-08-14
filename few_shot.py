c535f378aa4ba9581e51fba6dff118f3cbbfe346
Anuja khatri <70836354+Anujakhatri@users.noreply.github.com>
2025-08-14 09:35:55 +0545

    Update few_shot.py

--- few_shot.py
+++ few_shot.py
@@ -3,7 +3,7 @@ import json
 
 
 class FewShotPosts:
-    def __init__(self, file_path="data/processed_posts.json"):
+    def __init__(self, file_path = "data/processed_posts.json"):
         self.df = None
         self.unique_tags = None
         self.load_posts(file_path)
@@ -39,6 +39,5 @@ class FewShotPosts:
 
 if __name__ == "__main__":
     fs = FewShotPosts()
-    # print(fs.get_tags())
-    posts = fs.get_filtered_posts("Medium","Neplish","Job Search")
+    posts = fs.get_filtered_posts("Medium", "Neplish", "Job Search")
     print(posts)
